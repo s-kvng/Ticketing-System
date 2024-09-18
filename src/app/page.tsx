180e4c65115da5ffe4eaa4a55819d5afdead4c11
s-kvng <kvngnathan8420@gmail.com>
2024-09-18 03:53:53 +0000

    init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
 import Image from "next/image";
+import { getSupabaseBrowserClient } from "@/supabase-utils/browserClient";
 
 export default function Home() {
+  useEffect(() => {
+    const supabase = getSupabaseBrowserClient();
+    supabase.storage
+      .listBuckets()
+      .then((result) => console.log("Buckets list ", result));
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
