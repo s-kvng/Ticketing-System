04ed615a1627080f29b1bde069f9a3a7e9e9390a
s-kvng <kvngnathan8420@gmail.com>
2024-09-18 04:22:00 +0000

    setup: Req/Res client and middleware :microscope:
    add comments to Req/Res client file and add middleware template file

--- src/supabase-utils/reqResClient.js
+++ src/supabase-utils/reqResClient.js
@@ -1,19 +1,26 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse } from "next/server";
 
+// Supabase client setup and cookies manipulation
 export const getSupabaseReqResClient = ({ request }) => {
+  // Basically copies of the existing headers from the request and proceeds.
+  // we use the value property so supabase client can modify the response object later
   let response = {
     value: NextResponse.next({ request: request }),
   };
 
+  //   Supabase server client initialisation
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
+        // This function will be used to get all cookies from the request
         getAll() {
           return request.cookies.getAll();
         },
+        // This function will be used to set all cookies to the request
+        // To allow un-processed files such as a page , gain access to the cookies
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value, options }) => {
             request.cookies.set(name, value);
@@ -23,7 +30,10 @@ export const getSupabaseReqResClient = ({ request }) => {
     }
   );
 
+  //   Passing the new cookies to the response object
   response.value = NextResponse.next({ request });
+
+  //   setting the cookies to the response object
   cookiesToSet.forEach(({ name, value, options }) => {
     response.value.cookies.set(name, value, options);
   });
