9fd0d77563047195a3f22f4e52b016bb8d04ed51
s-kvng <kvngnathan8420@gmail.com>
2024-09-23 16:52:04 +0000

    logic: update middleware :100:

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,11 @@ import { getSupabaseReqResClient} from "./supabase-utils/reqResClient";
 
 export async function middleware(request){
     const { supabase, response }  = getSupabaseReqResClient({request});
+    await supabase.auth.getSession();
 
     return response.value
 }
+
+export const config = {
+    matcher: ["/((?!.*\\.).*)"],
+  }
