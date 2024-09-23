fdb4ef66bbd5024de3f04c705ad5c7aae0e58723
s-kvng <kvngnathan8420@gmail.com>
2024-09-23 16:54:29 +0000

    logic: Update middleware :100:

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,16 @@ import { getSupabaseReqResClient} from "./supabase-utils/reqResClient";
 
 export async function middleware(request){
     const { supabase, response }  = getSupabaseReqResClient({request});
+
+    // Check if user is authenticated by requesting the session , by doing that
+    // also refreshes the session...
     await supabase.auth.getSession();
 
     return response.value
 }
 
+// this matcher tells the middleware to avoid some static files eg. /favicon.ico 
+// so no to slow down your application
 export const config = {
     matcher: ["/((?!.*\\.).*)"],
   }
